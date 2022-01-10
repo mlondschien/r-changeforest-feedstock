330dc8c97b50838e9a64e1535602d23e3b620f3e
Malte Londschien <malte@londschien.de>
2022-01-10 17:45:00 +0100

    I have no idea what I am doing.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,7 @@
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
-echo "R ARGS:\n"
-echo ${R_ARGS}
-echo "R version\n"
-Rscript -e "v <- strsplit(R.version\$minor, '.', fixed = TRUE)[[1]];devel <- isTRUE(grepl('devel', R.version\$status, fixed = TRUE));cat(R.version\$major, v[1], paste0(v[2:length(v)], collapse = '.'), devel, R.version\$version.string, sep = '\n')"
-echo "\n\n"
-Rscript -e "R.version"
+echo "v <- strsplit(R.version\$minor, '.', fixed = TRUE)[[1]];devel <- isTRUE(grepl('devel', R.version\$status, fixed = TRUE));cat(R.version\$major, v[1], paste0(v[2:length(v)], collapse = '.'), devel, R.version\$version.string, sep = '\n')" > tmp.R
+Rscript tmp.R
 exit 1
 ${R} CMD INSTALL --build changeforest-r ${R_ARGS}
