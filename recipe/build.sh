da7874ad108ac2c8208dc4ae1a2b596c20530be6
Malte Londschien <malte@londschien.de>
2022-01-10 18:10:22 +0100

    Revert.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
-echo "v <- strsplit(R.version\$minor, '.', fixed = TRUE)[[1]];devel <- isTRUE(grepl('devel', R.version\$status, fixed = TRUE));cat(R.version\$major, v[1], paste0(v[2:length(v)], collapse = '.'), devel, R.version\$version.string, sep = '\n')" > tmp.R
-Rscript tmp.R
-exit 1
 ${R} CMD INSTALL --build changeforest-r ${R_ARGS}
