27a6802136e08388a2a61069ced6c9fe090e2d07
Malte Londschien <malte@londschien.de>
2022-01-10 17:27:50 +0100

    Even more monkey debugging.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,11 @@
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
+echo "R ARGS:\n"
 echo ${R_ARGS}
-Rscript -e ${R_ARGS} "v <- strsplit(R.version\$minor, '.', fixed = TRUE)[[1]];devel <- isTRUE(grepl('devel', R.version\$status, fixed = TRUE));cat(R.version\$major, v[1], paste0(v[2:length(v)], collapse = '.'), devel, R.version\$version.string, sep = '\n')"
+echo "R version\n"
+Rscript -e "v <- strsplit(R.version\$minor, '.', fixed = TRUE)[[1]];devel <- isTRUE(grepl('devel', R.version\$status, fixed = TRUE));cat(R.version\$major, v[1], paste0(v[2:length(v)], collapse = '.'), devel, R.version\$version.string, sep = '\n')"
+echo "\n\n"
+Rscript -e "R.version"
 exit 1
 ${R} CMD INSTALL --build changeforest-r ${R_ARGS}
