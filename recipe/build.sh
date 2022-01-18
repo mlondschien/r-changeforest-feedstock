ec302d2e7a646ea13f2099d37b25e3e35834510e
Malte Londschien <malte@londschien.de>
2022-01-18 22:12:52 +0100

    Try something.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,9 @@
 export DISABLE_AUTOBREW=1
 
+sed -i'' -e 's/https:\/\/github.com\/extendr\/extendr/https:\/\/github.com\/mlondschien\/extendr/g' changeforest-r/src/rust/Cargo.toml
+sed -i'' -e 's/6935c7dee0edc7e02c6355379376d8001167cd13/42f2b96ea1d5650b8f137d3d33d2a9f2806d02d3/g' changeforest-r/src/rust/Cargo.toml
+
+export R_VERSION=$(Rscript -e "v=R.Version(); cat(paste0(v\$major, '.', v\$minor))")
+
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build changeforest-r ${R_ARGS}
