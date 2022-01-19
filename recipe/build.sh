012e1872ebbe363a8d13fe68ef1c6080f3567101
Malte Londschien <malte@londschien.de>
2022-01-19 16:43:00 +0100

    Typo.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 export DISABLE_AUTOBREW=1
 
 mv /home/conda/feedstock_root/Cargo.toml changeforest-r/src/rust/Cargo.toml
-echo -e "use extendr_api::Operators;\n$(cat changeforest-r/src/rust/src/control.rs)" > (cat changeforest-r/src/rust/src/control.rs)
+echo -e "use extendr_api::Operators;\n$(cat changeforest-r/src/rust/src/control.rs)" > changeforest-r/src/rust/src/control.rs
 
 export R_VERSION=$(${R}script -e "v=R.Version(); cat(paste0(v\$major, '.', v\$minor, '\n'))")
 
