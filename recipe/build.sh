c78a2957bcf784221df58acb63f55b15e534a682
Malte Londschien <malte@londschien.de>
2022-01-19 17:11:13 +0100

    Use

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 export DISABLE_AUTOBREW=1
 
-mv /home/conda/feedstock_root/Cargo.toml changeforest-r/src/rust/Cargo.toml
+mv $RECIPE_DIR/Cargo.toml changeforest-r/src/rust/Cargo.toml
 echo -e "use extendr_api::Operators;\n$(cat changeforest-r/src/rust/src/control.rs)" > changeforest-r/src/rust/src/control.rs
 
 export R_VERSION=$(${R}script -e "v=R.Version(); cat(paste0(v\$major, '.', v\$minor, '\n'))")
