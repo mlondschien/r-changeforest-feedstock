5c749f5f47260f29af76a6afa0a040396fd4ed44
Malte Londschien <malte@londschien.de>
2022-01-19 10:34:36 +0100

    Try Ilias fork.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,9 @@
 export DISABLE_AUTOBREW=1
 
 sed -i'' -e 's/https:\/\/github.com\/extendr\/extendr/https:\/\/github.com\/mlondschien\/extendr/g' changeforest-r/src/rust/Cargo.toml
-sed -i'' -e 's/6935c7dee0edc7e02c6355379376d8001167cd13/42f2b96ea1d5650b8f137d3d33d2a9f2806d02d3/g' changeforest-r/src/rust/Cargo.toml
+sed -i'' -e 's/6935c7dee0edc7e02c6355379376d8001167cd13/8e26bf66ced8216a30eed787b4b0261dc7d3872f/g' changeforest-r/src/rust/Cargo.toml
 
-echo "v=R.Version(); cat(paste0(v\$major, '.', v\$minor))" > _tmp.r
-export R_VERSION=$(${R} _tmp.R)
+export R_VERSION=$(${R}script -e "v=R.Version(); cat(paste0(v\$major, '.', v\$minor, '\n'))")
 
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build changeforest-r ${R_ARGS}
