1bd2ef230cd1d239da60270279582b126d54fa10
Malte Londschien <malte@londschien.de>
2022-01-19 12:56:15 +0100

    New rev.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 export DISABLE_AUTOBREW=1
 
 sed -i'' -e 's/https:\/\/github.com\/extendr\/extendr/https:\/\/github.com\/mlondschien\/extendr/g' changeforest-r/src/rust/Cargo.toml
-sed -i'' -e 's/6935c7dee0edc7e02c6355379376d8001167cd13/91ad0f38f1363aecfb16bf9ba71f2f7da1735719/g' changeforest-r/src/rust/Cargo.toml
+sed -i'' -e 's/6935c7dee0edc7e02c6355379376d8001167cd13/bb6344e040e378c905918c026fe96c2ae5e05a4e/g' changeforest-r/src/rust/Cargo.toml
 
 export R_VERSION=$(${R}script -e "v=R.Version(); cat(paste0(v\$major, '.', v\$minor, '\n'))")
 
