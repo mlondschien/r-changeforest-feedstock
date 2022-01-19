bd668517976fa18b5f50ce0fc18a91c00299c8c1
Malte Londschien <malte@londschien.de>
2022-01-19 16:32:24 +0100

    Try something else.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 export DISABLE_AUTOBREW=1
 
-sed -i'' -e 's/https:\/\/github.com\/extendr\/extendr/https:\/\/github.com\/mlondschien\/extendr/g' changeforest-r/src/rust/Cargo.toml
-sed -i'' -e 's/6935c7dee0edc7e02c6355379376d8001167cd13/bb6344e040e378c905918c026fe96c2ae5e05a4e/g' changeforest-r/src/rust/Cargo.toml
+mv /home/conda/feedstock_root/Cargo.toml changeforest-r/src/rust/Cargo.toml
+echo -e "use extendr_api::Operators;\n$(cat changeforest-r/src/rust/src/control.rs)" > (cat changeforest-r/src/rust/src/control.rs)
 
 export R_VERSION=$(${R}script -e "v=R.Version(); cat(paste0(v\$major, '.', v\$minor, '\n'))")
 
