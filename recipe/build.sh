0e96997950e5d65d48c16c58594954d29b626864
Malte Londschien <malte@londschien.de>
2024-01-07 19:54:35 +0100

    Set TARGET environment on osx-arm64 build.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,10 @@
 export DISABLE_AUTOBREW=1
 
+# https://github.com/conda-forge/r-changeforest-feedstock/issues/28
+if [[ $target_platform == osx-64 ]]; then
+  export TARGET="aarch64-apple-darwin"
+fi
+
 # Pass R version to libR-sys package via environment variable. This allows for cross-compilation,
 # and thus osx-arm64 builds. See also https://github.com/extendr/libR-sys/issues/85.
 export LIBRSYS_R_VERSION=$(${R}script -e "v=R.Version(); cat(paste0(v\$major, '.', v\$minor, '\n'))")
